79b74d910149eae43a10dc5735d534eda47c162a
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-06-30 10:16:04 +0530

    Add files via upload

---
+++ stuff.py
@@ -0,0 +1,21 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    check, image = cam.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Find bounding box
+    x,y,w,h = cv2.boundingRect(thresh)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+    cv2.putText(image, "w={},h={}".format(w,h), (x,y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (36,255,12), 2)
+    cv2.imshow('video', image)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
+
